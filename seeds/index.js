0e973194eaf4f2c0c8d16e7cc0ccf99bf2a30bb1
dltorrise <dltorrise@gmail.com>
2023-03-15 19:23:08 -0500

    shoe seeds

--- seeds/index.js
+++ seeds/index.js
@@ -7,7 +7,7 @@ const seedUserProducts = require('./user-product-seeds');
 const seedUsers = require('./user-seeds');
 const sequelize = require('../config/connection');
 const seedAll = async () => {
-  await sequelize.sync({ force: true });
+  await sequelize.sync({ force: true }); //will delete old ones
   console.log('\n----- DATABASE SYNCED -----\n');
   await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
